cf9274c00107426e4b030c3e8ea1a5bb0ace81c2
Aayush Katiyar <aayush.iosdeveloper@gmail.com>
2023-08-08 21:54:22 +0530

    second commit

--- hello-world/src/components/Cart.vue
+++ hello-world/src/components/Cart.vue
@@ -70,6 +70,32 @@
         </div>
       </div>
     </div>
+    <div v-show="showCardModal" class="credit-card-form">
+      <h2>Credit Card Payment</h2>
+      <form>
+        <div class="form-group">
+          <label for="card-number">Card Number</label>
+          <input type="text" id="card-number" placeholder="Card number" />
+        </div>
+        <div class="form-group">
+          <label for="card-holder">Card Holder</label>
+          <input type="text" id="card-holder" placeholder="Card holder's name" />
+        </div>
+        <div class="form-row">
+          <div class="form-group form-column">
+            <label for="expiry-date">Expiry Date</label>
+            <input type="text" id="expiry-date" placeholder="MM/YY" />
+          </div>
+          <div class="form-group form-column">
+            <label for="cvv">CVV</label>
+            <input type="text" id="cvv" placeholder="CVV" />
+          </div>
+        </div>
+        <button type="submit" class="click-button" v-on:click="showLoading(event, this)">
+          Pay Now
+        </button>
+      </form>
+    </div>
     <Footer />
   </div>
 </template>
@@ -88,6 +114,7 @@ export default {
   data() {
     return {
       item: 0,
+      showCardModal: false,
     };
   },
   methods: {
@@ -98,10 +125,27 @@ export default {
       this.$store.dispatch("removeItem", item);
     },
     checkout() {
-      swal("Good job!", "Your order is placed successfully!", "success").then((value) => {
-        window.location.href = "/cart";
-        console.log(value);
-      });
+      this.showCardModal = true;
+      // swal("Good job!", "Your order is placed successfully!", "success").then((value) => {
+      //   window.location.href = "/cart";
+      //   console.log(value);
+      // });
+    },
+    showLoading(event, button) {
+      event.preventDefault(); // Prevent form submission
+
+      button.innerHTML = "Processing Payment...";
+
+      setTimeout(function () {
+        button.innerHTML = "Payment completed.";
+      }, 3000).then(() => {
+        swal("Good job!", "Your order is placed successfully!", "success").then(
+          (value) => {
+            window.location.href = "/cart";
+            console.log(value);
+          }
+        );
+      }); // Change to the desired duration in milliseconds
     },
   },
   computed: {
@@ -195,5 +239,98 @@ input[type="text"] {
 }
 .darkblue {
   color: darkblue;
+}
+@import url("https://fonts.googleapis.com/css2?family=DM+Sans:wght@500&family=Montserrat:wght@600&display=swap");
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #686868;
+  align-content: center;
+  text-align: center;
+}
+
+.credit-card-form {
+  margin-top: 10%;
+  margin-left: 35%;
+  max-width: 400px;
+  padding: 1em;
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(255, 255, 255, 0.1);
+  font-family: "Montserrat", sans-serif;
+  background-color: #dbdbdb;
+  text-align: center;
+  color: #004187;
+  align-content: center;
+}
+
+.credit-card-form h2 {
+  margin-bottom: 10%;
+  font-size: 24px;
+}
+
+.credit-card-form .form-group {
+  margin-bottom: 15px;
+}
+
+.credit-card-form label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 5px;
+  color: #004187;
+}
+
+.credit-card-form input[type="text"],
+.credit-card-form select {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ddd;
+  border-radius: 1rem;
+  font-size: 16px;
+  font-family: "Montserrat", sans-serif;
+}
+
+.credit-card-form .form-row {
+  display: flex;
+}
+
+.credit-card-form button[type="submit"] {
+  width: 100%;
+  padding: 14px;
+  background-color: #585858;
+  color: #fff;
+  border: none;
+  border-radius: 1rem;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s ease;
+  font-family: "Montserrat", sans-serif;
+}
+
+.credit-card-form button[type="submit"]:hover {
+  background-color: #bebebe;
+  color: #424242;
+  font-family: "Montserrat", sans-serif;
+}
+
+.credit-card-form button[type="submit"]:focus {
+  outline: none;
+  font-family: "Montserrat", sans-serif;
+}
+
+p {
+  color: white;
+  margin-top: 6%;
+  font-family: "Montserrat", sans-serif;
+  text-align: center;
+  margin-bottom: 45px;
+  font-size: 70%;
+  text-shadow: 0 0 5px #cacaca;
 }
 </style>
